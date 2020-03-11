d3d7700a84aba672422624603d792accb1c3ce07
payam394 <payam.ak394@gmail.com>
2020-03-11 10:39:29 +0100

    maj

---
+++ src/main/java/Personnel.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+
+public class Personnel {
+	
+	private String nom;
+	private String prenom;
+	private String telephone;
+	
+	private LocalDate dateDeNaissance;
+	
+
+	
+}
