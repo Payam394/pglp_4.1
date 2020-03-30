798425b6dbf686164b4fa995df45e079e4c1aba0
payam394 <payam.ak394@gmail.com>
2020-03-30 19:22:19 +0200

    mise a jour Personnel

--- src/main/java/Personnel.java
+++ src/main/java/Personnel.java
@@ -1,14 +1,71 @@
 import java.time.LocalDate;
 
 
-public class Personnel {
+public class Personnel implements Groupe{
+	
 	
 	private String nom;
 	private String prenom;
+	private String ID;
+	
 	private String telephone;
 	
 	private LocalDate dateDeNaissance;
 	
+	public static class Builder {
+		
+		private String nom;
+		private String prenom;
+		private String ID;
+		
+		private String telephone;
+		
+		private LocalDate dateDeNaissance;
+		
+		public Builder (String nom, String prenom) {
+			this.nom=nom;
+			this.prenom=prenom;
+		}
+		
+		public Builder (String ID) {
+			this.ID=ID;
+		}
+		
+		public Builder withTelephone(String tel) {
+			this.telephone=tel;
+			return this;
+		}
+		
+		public Builder withDateN(LocalDate date) {
+			this.dateDeNaissance=date;
+			return this;
+		}
+		
+		
+		public Personnel build () {
+			Personnel p= new Personnel(this);
+			
+			return p;
+		}
+		
+	}
+	
 
+	
+	private Personnel(Builder personnels) {
+		
+		this.nom=personnels.nom;
+		this.prenom=personnels.prenom;
+		this.ID=personnels.ID;
+		this.dateDeNaissance=personnels.dateDeNaissance;
+		this.telephone=personnels.telephone;
+	
+	}
+	
+	public void print() {
+		System.out.println("Personnel");
+		
+	}
+	
 	
 }
