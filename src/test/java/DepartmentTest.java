9f1d1061421b44d0ebdc01baeae6da05100ba976
payam394 <payam.ak394@gmail.com>
2020-05-24 18:38:45 +0200

    tests completes

--- src/test/java/DepartmentTest.java
+++ src/test/java/DepartmentTest.java
@@ -1,5 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.junit.jupiter.api.Test;
 
 class DepartmentTest {
@@ -14,39 +17,50 @@ class DepartmentTest {
 	Groupe g3;
 	Groupe g4;
 	
-	
+	ArrayList<Department> Arr = new ArrayList<Department>();
 	
 	
 
 	@Test
 	void test1() {
-		p1 = new Personnel.Builder("Akrami", "Payam", 1).build();
-		p2 = new Personnel.Builder("Zacki", "Marc", 2).build();
-		p3 = new Personnel.Builder("Gates", "Bill", 3).build();
-		p4 = new Personnel.Builder("Bezos", "Jeff", 4).build();
 		
-		p1.print();
-		p2.print();
-		p3.print();
-		p4.print();
-		System.out.println();
-	
-		g1 = new Groupe("alpha");
-		g1.addTo(p1);
-		g1.addTo(p2);
+
+			p1 = new Personnel.Builder("Akrami", "Payam", 1).build();
+			p2 = new Personnel.Builder("Zacki", "Marc", 2).build();
+			p3 = new Personnel.Builder("Gates", "Bill", 3).build();
+			p4 = new Personnel.Builder("Bezos", "Jeff", 4).build();
+			
 		
-		g2 = new Groupe("betta");
-		g2.addTo(g1);
-		g2.addTo(p3);
-		
-		g4 = new Groupe ("tetta");
-		g4.addTo(p4);
-		
-		g3 = new Groupe("gamma");
-		g3.addTo(g2);
-		g3.addTo(g1);
-		
-		g3.print();
+			g1 = new Groupe("alpha");
+			g1.addTo(p1);
+			g1.addTo(p2);
+			
+			g2 = new Groupe("betta");
+			g2.addTo(g1);
+			g2.addTo(p3);
+			
+			g4 = new Groupe ("tetta");
+			g4.addTo(p4);
+			
+			g3 = new Groupe("gamma");
+			g3.addTo(g2);
+			g3.addTo(g1);
+			
+			
+			
+			Arr.add(g3);
+			
+			affchParGrp p = new affchParGrp(Arr);
+			
+
+			
+			for(Iterator<Department> iter = p.getIterator(); iter.hasNext();){
+		         Department name = iter.next();
+		         name.print();
+		      } 
+			
+
+			
 	}
 
 }
